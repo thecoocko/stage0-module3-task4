db6785c0033df09d08a01913e8b71c230587a6d4
thecoocko <butenkonaddezhda@gmail.com>
2023-06-09 16:48:40 +0200

    done

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        //c99h104a97r114
+
         System.out.println((char)99);
         System.out.println((char)104);
         System.out.println((char)97);
