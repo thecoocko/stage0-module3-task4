bac481c6e21152c5892c1294c93b2134de317502
thecoocko <butenkonaddezhda@gmail.com>
2023-06-09 16:44:38 +0200

    done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a=1;
+        int b=1;
+        double formula = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+        System.out.println(formula);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        //c99h104a97r114
+        System.out.println((char)99);
+        System.out.println((char)104);
+        System.out.println((char)97);
+        System.out.println((char)114);
     }
 }
